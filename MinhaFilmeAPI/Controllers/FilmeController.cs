9ee18d328b89ce543f06eed73c62d31c79d8ffa9
Nicholas Barbosa <nicholasbarbosads2001@gmail.com>
2025-10-27 01:45:43 -0300

    Criação da rota post

---
+++ MinhaFilmeAPI/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaFilmeAPI.Models;
+
+namespace MinhaFilmeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.DuracaoEmMinutos);
+        }
+    }
+}
---
+++ MinhaFilmeAPI/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinhaFilmeAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O título do filme é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O gênero do filme não pode exceder 50 caracteres")]
+        public string Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duração do filme deve estar entre 70 e 600 minutos")]
+        public int DuracaoEmMinutos { get; set; }
+    }
+}
